14d8e45249ea3412660117523d9e1c78d6694198
Caleb Stanford <caleb.pirsquared@gmail.com>
2024-02-01 14:55:13 -0800

    :wrench: progress

--- fish-friendly/src/bin/count.rs
+++ fish-friendly/src/bin/count.rs
@@ -18,18 +18,27 @@ struct Args {
 pub fn main() {
     let args = Args::parse();
 
-    let mut total: usize = 0;
-    let mut friendly: usize = 0;
+    let expect_total: u128 = 2_u128.checked_pow((args.rows * args.cols) as u32).expect(
+        "The number of grids is too large to fit in a u128"
+    );
+    let progress_step = expect_total / 10;
+
+    let mut total: u128 = 0;
+    let mut friendly: u128 = 0;
     let mut grid = Grid::new_empty(args.rows, args.cols);
     loop {
         if grid.fish_friendly() {
             friendly += 1;
         }
         total += 1;
+        if total % progress_step == 0 {
+            println!("Progress: {:.0}%", (total as f64) * 100.0 / (expect_total as f64));
+        }
         if !grid.rotate() {
             break;
         }
     }
+    debug_assert_eq!(total, expect_total);
 
     println!("=== Results for {} x {} grids ===", args.rows, args.cols);
     println!(
