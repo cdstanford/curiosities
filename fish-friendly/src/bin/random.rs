77b981cac6ad79c94feec392b02edfda26aba247
Caleb Stanford <caleb.pirsquared@gmail.com>
2024-02-01 13:31:25 -0800

    :wrench: add CLI

--- fish-friendly/src/bin/random.rs
+++ fish-friendly/src/bin/random.rs
@@ -5,31 +5,45 @@
 
 use fish_friendly::Grid;
 
+use clap::Parser;
 use std::io::{self, Write};
 
-// Number of iterations to run
-const NUM_ITERS: usize = 1000000;
 // Number of steps to display progress
-// (Should be a divisor of NUM_ITERS)
-const PROGRESS_STEP: usize = 100000;
+const PROGRESS_STEPS: usize = 10;
 
-const ROWS: usize = 10;
-const COLS: usize = 10;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(help = "Number of rows")]
+    rows: usize,
+    #[arg(help = "Number of columns")]
+    cols: usize,
+    #[arg(short, long, help = "Number of iterations", default_value = "1000000")]
+    num_iters: usize,
+}
 
 fn main() {
-    println!("Running {} iterations for {} x {} grids", NUM_ITERS, ROWS, COLS);
+    let args = Args::parse();
+
+    if args.num_iters % PROGRESS_STEPS != 0 {
+        eprintln!("The number of iterations should be a multiple of {}", PROGRESS_STEPS);
+        std::process::exit(1);
+    }
+    let progress_step = args.num_iters / 10;
+
+    println!("Running {} iterations for {} x {} grids", args.num_iters, args.rows, args.cols);
     let mut friendly: usize = 0;
-    for epoch in 0..(NUM_ITERS / PROGRESS_STEP) {
+    for epoch in 0..(args.num_iters / progress_step) {
         print!(
             "Running iterations {}-{}...",
-            epoch * PROGRESS_STEP,
-            (epoch + 1) * PROGRESS_STEP - 1,
+            epoch * progress_step,
+            (epoch + 1) * progress_step - 1,
         );
         io::stdout().flush().unwrap();
 
         let mut new_friendly: usize = 0;
-        for _ in 0..PROGRESS_STEP {
-            let grid = Grid::new_random(ROWS, COLS);
+        for _ in 0..progress_step {
+            let grid = Grid::new_random(args.rows, args.cols);
             if grid.fish_friendly() {
                 new_friendly += 1;
             }
@@ -38,11 +52,11 @@ fn main() {
         friendly += new_friendly;
     }
 
-    println!("=== Results for {} x {} grids ===", ROWS, COLS);
+    println!("=== Results for {} x {} grids ===", args.rows, args.cols);
     println!(
         "The fish can swim across in {}/{} cases ({:.3}%).",
         friendly,
-        NUM_ITERS,
-        (friendly as f64) * 100.0 / (NUM_ITERS as f64),
+        args.num_iters,
+        (friendly as f64) * 100.0 / (args.num_iters as f64),
     );
 }
