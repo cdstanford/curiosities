a94abac937d48e79d9355183aef54bc589e2a3cf
Caleb Stanford <caleb.pirsquared@gmail.com>
2024-02-01 19:26:16 -0800

    :wrench: upd: order by rows + cols

--- fish-friendly/src/bin/table.rs
+++ fish-friendly/src/bin/table.rs
@@ -10,41 +10,42 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(help = "Up to number of rows")]
-    rows_upto: usize,
-    #[arg(help = "Up to number of columns")]
-    cols_upto: usize,
+    #[arg(help = "Up to size (sum of rows + cols)")]
+    upto: usize,
 }
 
 #[allow(clippy::needless_range_loop)]
 pub fn main() {
-    let args = Args::parse();
+    let Args { upto } = Args::parse();
 
     let mut friendly_table =
-        vec![vec![0_u128; args.cols_upto + 1]; args.rows_upto + 1];
+        vec![vec![None; upto]; upto];
     let mut total_table =
-        vec![vec![0_u128; args.cols_upto + 1]; args.rows_upto + 1];
-    for rows in 1..=args.rows_upto {
-        for cols in 1..=args.cols_upto {
+        vec![vec![None; upto]; upto];
+    for size in 1..upto {
+        for rows in 1..size {
+            let cols = size - rows;
             println!("=== {} x {} grids ===", rows, cols);
             let (friendly, total) = count_friendly_grids(rows, cols);
-            friendly_table[rows][cols] = friendly;
-            total_table[rows][cols] = total;
+            friendly_table[rows][cols] = Some(friendly);
+            total_table[rows][cols] = Some(total);
         }
     }
 
     println!("=== Results ===");
     println!("Friendly:");
-    for row in 1..=args.rows_upto {
-        for col in 1..=args.cols_upto {
-            print!("{:10}", friendly_table[row][col]);
+    for row in 1..upto {
+        for col in 1..upto {
+            let s = friendly_table[row][col].map(|x| format!("{:10}", x)).unwrap_or("".to_string());
+            print!("{}", s);
         }
         println!();
     }
     println!("Total:");
-    for row in 1..=args.rows_upto {
-        for col in 1..=args.cols_upto {
-            print!("{:10}", total_table[row][col]);
+    for row in 1..upto {
+        for col in 1..upto {
+            let s = total_table[row][col].map(|x| format!("{:10}", x)).unwrap_or("".to_string());
+            print!("{}", s);
         }
         println!();
     }
